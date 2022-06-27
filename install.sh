14bfcc3ebd8c5e3e4d85cfc7c2d2682745a1e635
domecca <57264796+domecca@users.noreply.github.com>
2022-06-27 12:51:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,17 @@
 yum clean all
 yum update
 
-# Install Apache
-yum install httpd
-
-# Start Apache
-systemctl start httpd.service
-
 # Stop Firewall
 systemctl stop firewalld
 
 # OR Disable Firewall
 systemctl disable firewalld
 
-# Adds Group Development Tools
-yum groupinstall "Development tools"
+# Adds Group Development Tools, PHP, Perl, Apache
+yum groupinstall "Development tools" patch gcc httpd kernel-headers kernel-devel make perl wget php php-gd php-common php-soap php-mysql perl-libwww-perl.noarch perl-LWP-Protocol-https.noarch perl-GD perl-GDGraph net-tools perl-LWP-Protocol-https gd-devel pam-devel
+
+# Start Apache
+systemctl start httpd.service
 
 # Enable SSH on boot
 systemctl start sshd
@@ -26,9 +23,9 @@ systemctl start mariadb
 mysql_secure_installation
 systemctl enable mariadb.service
 
-# Install PHP
-yum install php php-mysql
-systemctl restart httpd.service
+# Reboot your system to start using the new kernel
+reboot
+
 
 # Install Webmin
 nano /etc/yum.repos.d/webmin.repo
@@ -45,10 +42,6 @@ wget http://software.virtualmin.com/gpl/scripts/install.sh
 chmod +x install.sh
 ./install.sh
 
-# Install Perl
-yum install perl
-yum install perl-libwww-perl.noarch perl-LWP-Protocol-https.noarch perl-GD perl-GDGraph net-tools perl-LWP-Protocol-https gd-devel pam-devel
-
 #Install ConfigServer
 cd /usr/src
 rm -fv csf.tgz
@@ -56,5 +49,32 @@ wget https://download.configserver.com/csf.tgz
 tar -xzf csf.tgz
 cd csf
 sh install.sh
+
+# Install Virtualbox
+wget http://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -P /etc/yum.repos.d
+yum install VirtualBox-6.0
+
+# Check if Virtualbox is running
+systemctl status vboxdrv
+useradd vboxweb
+passwd vboxweb
+usermod -a -G vboxusers vboxweb
+cd /tmp/ && wget https://github.com/phpvirtualbox/phpvirtualbox/archive/master.zip
+unzip master.zip -d /var/www/html/
+cd /var/www/html/ && mv phpvirtualbox-master/ phpvirtualbox/
+cd phpvirtualbox/
+cp config.php-example config.php
+nano config.php
+# Change the username/password to the user that runs the VirtualBox web service.
+
+var $username = 'vboxweb';
+var $password = 'pass';
+
+echo "VBOXWEB_USER=vboxweb" > /etc/default/virtualbox
+systemctl restart vboxweb-service
+yum install policycoreutils-python -y
+semanage port -a -t http_port_t -p tcp 18083
+
+
 
 
